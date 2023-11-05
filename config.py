ddad655bd4b23425c7411e95e354e8f274dfd164
cozybear <lukas.tillmann@student.unisg.ch>
2023-11-05 18:24:08 +0100

    config

---
+++ config.py
@@ -0,0 +1,20 @@
+# add your custom tokens below
+
+
+# telegram bot TOKENS
+token = ''
+token_test_enviroment = ''
+
+
+# telegram stripe payment provider token
+PAYMENT_PROVIDER_TOKEN = ''
+
+
+# crypto pay API token
+crypto_token_test = ''
+
+crypto_token_real = ''
+
+
+# private chat id
+private_chat_id =
